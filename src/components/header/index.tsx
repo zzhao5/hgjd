1ad99cf2c2644f5a13a0a8d15234d0e213a3a319
zz <zzhao5@qq.com>
2023-11-13 21:32:24 +0800

    feat: 优化导航事件

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import _s from './index.module.scss';
 import c from 'classnames';
@@ -44,31 +44,34 @@ const NAV_LIST = [
 
 const Header = () => {
 
+  const [showNav, setShowNav] = useState(false);
+  const [showSubNav, setShowSubNav] = useState(false);
+
+  useEffect(() => {
+    const handleClick = () => {
+      if (showNav) setShowNav(false);
+    }
+    window.addEventListener('click', handleClick);
+    return () => {
+      window.removeEventListener('click', handleClick);
+    }
+  }, [showNav]);
+
   return (
     <header className={_s.wrap}>
       <div className={_s.main}>
         <div className={_s.logo}>
           <img src={logo} alt="" />
         </div>
-        <div className={_s.nav}>
+        <div className={c(_s.nav, showNav ? _s.active : '')} onClick={(event) => event.stopPropagation()}>
           {
             NAV_LIST.map((item, index) => 
               // TODO: 这里的 end 没有起到理想的作用，导致 /service/list 时不能高亮 /service
               <NavLink key={index} className={({isActive}) => c(_s.item, isActive ? _s.active : '')} to={item.path} end={item.path === '/'} >
-                  <span onMouseEnter={(event) => {
-                    if (item.sub) {
-                      const sub = event.currentTarget.getElementsByClassName(_s.sub)[0] as HTMLElement;
-                      sub.style.display = 'block';
-                    }
-                  }} onMouseLeave={(event) => {
-                    if (item.sub) {
-                      const sub = event.currentTarget.getElementsByClassName(_s.sub)[0] as HTMLElement;
-                      sub.style.display = 'none';
-                    }
-                  }}>
+                  <span onMouseEnter={() => setShowSubNav(true)} onMouseLeave={() => setShowSubNav(false)}>
                     {item.name}
                     {
-                      item.sub && <div className={_s.sub} style={{display: 'none',}}>
+                      item.sub && <div className={c(_s.sub, showSubNav ? _s.active : '')}>
                         {
                           item.sub.map((subItem, subIndex) => 
                             <NavLink key={index + '-' + subIndex} to={subItem.path}>{subItem.name}</NavLink>
@@ -81,6 +84,7 @@ const Header = () => {
             )
           }
         </div>
+        <div className={_s.nav_button} onClick={(event) => {setShowNav(true); event.stopPropagation();}}></div>
       </div>
     </header>
   )
