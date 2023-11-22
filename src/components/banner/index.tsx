7d3a89e1b465902d0dfed879cdb7ec9868314a94
zz <zzhao5@qq.com>
2023-11-22 22:55:21 +0800

    feat: 优化 banner 模块

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -1,19 +1,40 @@
 import _s from './index.module.scss';
 import c from 'classnames';
+import pic1 from './images/pic_banner1.jpeg';
+import pic2 from './images/pic_banner2.jpeg';
+import pic3 from './images/pic_banner3.jpeg';
+import pic4 from './images/pic_banner4.jpeg';
+import pic5 from './images/pic_banner5.jpeg';
+import pic6 from './images/pic_banner6.jpeg';
+import { useMemo } from 'react';
 
+const PICS = [
+  { text: '消息', pic: pic1, },
+  { text: '关于', pic: pic2, },
+  { text: '服务', pic: pic3, },
+  { text: '经验', pic: pic4, },
+  { text: '专家', pic: pic5, },
+  { text: '联系', pic: pic6, },
+];
 
 const Banner = ({
   name,
-  pic,
 }: {
   name: string;
-  pic: string;
 }) => {
+  const idx = useMemo(() => {
+    let index = 0;
+    for( let i = 0; i < PICS.length; i++) {
+      if (name.indexOf(PICS[i].text) !== -1) {
+        index =i;
+        break;
+      }
+    }
+    return index;
+  }, [name]);
 
   return (
-    <div className={c(_s.banner)} style={{backgroundImage: `url(${pic})`,}}>
-      <div>{name}</div>
-    </div>
+    <div className={c(_s.banner)} data-name={name} style={{backgroundImage: `url(${PICS[idx].pic})`,}}></div>
   )
 }
 
